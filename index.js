22b33f155168e961a3543c84d7049aaf2a8a7e06
Peter Jankovsky <peter@littlepassports.com>
2016-05-24 16:28:51 -0700

    final processing

--- index.js
+++ index.js
@@ -15,11 +15,13 @@ let parseString = require('xml2js').parseString;
 let amazonSignature = require('tg-node-lib/lib/amazonSignature');
 let dbCatalog = require('tg-node-lib/lib/db/catalog');
 
-// numbers that need to be reset
+// vars that need to be reset
 let timeout = 0;
+let recentlyAddedASINs = {};
 
 exports.handler = (event, context, callback) => {
-    timeout = Date.now() + 270000; // 4m30s
+    timeout = Date.now() + 300000; // 5m
+    recentlyAddedASINs = {};
     // setup returns a promise
     // doing setup each instance might be overkill, but it is better than doing it every time a table is loaded
     dbCatalog.setup()
@@ -35,9 +37,9 @@ function poll() {
     return new Promise((resolve, reject) => {
         SQS.receiveMessage({
             QueueUrl: LOOKUP_QUEUE_URL,
-            MaxNumberOfMessages: 1,
-            // MaxNumberOfMessages: 10,
-            WaitTimeSeconds: 0
+            // MaxNumberOfMessages: 1,
+            MaxNumberOfMessages: 10,
+            WaitTimeSeconds: 1
         }, (err, data) => {
             if (err)
                 return reject(err);
@@ -68,11 +70,11 @@ function poll() {
 
             p.then(() => {
                 // poll again for more messages
-                // if (Date.now() < timeout) {
-                //     poll().then((res) => resolve(res)).catch((err) => reject(err));
-                // } else {
-                resolve('Done');
-                // }
+                if (Date.now() < timeout) {
+                    poll().then((res) => resolve(res)).catch((err) => reject(err));
+                } else {
+                    resolve('Done');
+                }
             }).catch((err) => reject(err));
         })
     });
@@ -80,12 +82,36 @@ function poll() {
 
 function deleteMessage(Message) {
     console.log('--- Delete Message');
-    // SQS.deleteMessage({
-    //     QueueUrl: LOOKUP_QUEUE_URL,
-    //     ReceiptHandle: Message.ReceiptHandle
-    // }, (err) => {
-    //     if (err) throw err;
-    // });
+    SQS.deleteMessage({
+        QueueUrl: LOOKUP_QUEUE_URL,
+        ReceiptHandle: Message.ReceiptHandle
+    }, (err) => {
+        if (err) throw err;
+    });
+}
+
+function addToLookupQueueIfNotExists(asin) {
+    return dbCatalog.Product().count({where: {asin: asin}})
+        .then((count) => {
+            if (count > 0) throw new Error('ASIN already exists, do not loop add it.');
+            if (recentlyAddedASINs[asin] == true) throw new Error('ASIN already queued');
+        })
+        .then(() => {
+            console.log('---- Queue ASIN -> ' + asin);
+            return new Promise((resolve) => {
+                SQS.sendMessage({
+                    MessageBody: asin,
+                    QueueUrl: LOOKUP_QUEUE_URL
+                }, (err) => {
+                    if (err)
+                        throw err;
+                    resolve(true);
+                });
+            });
+        })
+        .catch(() => {
+            return false;
+        });
 }
 
 function lookup(asin) {
@@ -99,7 +125,7 @@ function lookup(asin) {
             IdType: 'ASIN',
             ItemId: asin,
             Operation: 'ItemLookup',
-            ResponseGroup: 'Large',
+            ResponseGroup: 'Accessories,BrowseNodes,EditorialReview,Images,ItemAttributes,Request,SalesRank,Similarities,Small',
             Timestamp: amazonSignature.getSigningTimestamp()
         };
 
@@ -127,7 +153,6 @@ function lookup(asin) {
         req.on('error', (err) => reject(err));
         req.end();
     }).then((result) => {
-
         if (typeof result.ItemLookupResponse === 'undefined'
             ||
             typeof result.ItemLookupResponse.Items === 'undefined'
@@ -138,12 +163,29 @@ function lookup(asin) {
         ) {
             return false; // something went wrong, keep it in the queue
         }
-
         // promise
+        console.log('---- Save Item');
         return dbCatalog.importAmazonItem(result.ItemLookupResponse.Items.Item);
     }).then((product) => {
         if (product === false)
             return false;
-        // so anything else
+        // add related asins to the queue
+        var newAsins = [];
+        if (typeof product.amzItem.SimilarProducts != 'undefined') {
+            for (var i = 0; i < product.amzItem.SimilarProducts.SimilarProduct.length; i++) {
+                newAsins.push(addToLookupQueueIfNotExists(product.amzItem.SimilarProducts.SimilarProduct[i].ASIN));
+            }
+        }
+        if (typeof product.amzItem.Accessories != 'undefined') {
+            for (var i = 0; i < product.amzItem.Accessories.Accessory.length; i++) {
+                newAsins.push(addToLookupQueueIfNotExists(product.amzItem.Accessories.Accessory[i].ASIN));
+            }
+        }
+        return Promise.all(newAsins);
+    }).then(() => {
+        return true;
+    }).catch((err) => {
+        console.log(err);
+        return false;
     });
 }
